a8c493b1f97a65d3205ab2d625c14b67bf116135
Colabear754 <jky2406@gmail.com>
2022-09-04 00:31:14 +0900

    css, js 수정

--- src/main/webapp/resources/admin/js/noticelist_script.js
+++ src/main/webapp/resources/admin/js/noticelist_script.js
@@ -7,4 +7,10 @@ $(function() {
 	}, function() {
 		$(this).removeClass('hover');
 	})
+	
+	$('.title').hover(function() {
+		$(this).addClass('title_hover')
+	}, function() {
+		$(this).removeClass('title_hover')
+	})
 })
