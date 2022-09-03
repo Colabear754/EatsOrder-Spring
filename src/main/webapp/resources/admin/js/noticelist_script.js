0daf3447cf0ba74414a210955cca5708925a892e
Colabear754 <jky2406@gmail.com>
2022-09-04 00:50:17 +0900

    관리자 공지 목록 css, js 수정

--- src/main/webapp/resources/admin/js/noticelist_script.js
+++ src/main/webapp/resources/admin/js/noticelist_script.js
@@ -13,4 +13,18 @@ $(function() {
 	}, function() {
 		$(this).removeClass('title_hover')
 	})
+	
+	var category = $('#category').val()
+	
+	switch (Number(category)) {
+	case 1:
+		$('#empty').text('작성된 공지사항이 없습니다.')
+		break;
+	case 2:
+		$('#empty').text('진행중인 이벤트가 없습니다.')
+		break;
+	case 3:
+		$('#empty').text('등록된 FAQ가 없습니다.')
+		break;
+	}
 })
